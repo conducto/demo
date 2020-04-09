7f5a1f4a2694aa775c6837ec6c82dd8aba18e3e0
Jonathan Marcus <jmarcus@conducto.com>
2020-04-09 12:08:35 -0400

    additional documentation in data_science/intro.py

--- data_science/intro.py
+++ data_science/intro.py
@@ -20,12 +20,15 @@ DATASETS = {
 IMG = co.Image("python:3.8", copy_dir=".", reqs_py=["conducto", "pandas", "matplotlib", "tabulate"])
 
 
-def intro() -> co.Serial:
+def run() -> co.Serial:
+    """
+    Pipeline that downloads data from the US EIA and visualizes it.
+    """
     with co.Serial(image=IMG) as output:
         # First download some data from the US Energy Information Administration.
         output["Download"] = co.Exec(DOWNLOAD_COMMAND)
 
-        # Then display
+        # Then make a few different visualizations of it.
         output["Display"] = co.Parallel()
         for dataset in DATASETS.keys():
             output["Display"][dataset] = co.Exec(f"python intro.py display --dataset='{dataset}'")
@@ -33,6 +36,9 @@ def intro() -> co.Serial:
 
 
 def display(dataset):
+    """
+    Read in the downloaded data, extract the specified datasets, and plot them.
+    """
     data_text = co.perm_data.gets(PERM_DATA_PATH)
     all_data = [json.loads(line) for line in data_text.splitlines()]
 
@@ -44,7 +50,9 @@ def display(dataset):
     import pandas as pd
     import numpy as np
 
-    # Create a pandas DataFrame with the data grouped by month of the year
+    # Create a pandas DataFrame with the data grouped by month of the year. This could
+    # be implemented with vectorized pandas logic but this data is small enough not to
+    # worry.
     data = {}
     for i, d in enumerate(subset_data):
         by_month = collections.defaultdict(list)
@@ -58,7 +66,7 @@ def display(dataset):
     df["Month"] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     df.set_index("Month", inplace=True)
 
-    # Plot it
+    # Graph each dataset as one line on a single plot.
     colors = [cm.viridis(z) for z in np.linspace(0, .99, len(subset_data))]
     for i, column in enumerate(df.columns):
         y = df[column].values
@@ -66,12 +74,13 @@ def display(dataset):
     plt.title(f"{dataset}, average by month")
     plt.legend(loc="best", fontsize="x-small")
 
+    # Save to disk, and then to co.temp_data
     filename = "/tmp/image.png"
+    dataname = f"conducto/demo/data_science/{dataset}.png"
     plt.savefig(filename)
-
-    dataname = f"conducto/demo/data_science/{dataset.lower().replace(' ', '_')}.png"
     co.temp_data.put(dataname, filename)
 
+    # Print out results as markdown
     print(f"""
 <ConductoMarkdown>
 ![img]({co.temp_data.url(dataname)})
@@ -82,4 +91,4 @@ def display(dataset):
 
 
 if __name__ == "__main__":
-    co.main(default=intro)
+    co.main()
